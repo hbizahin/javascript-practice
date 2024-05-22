2f003f9cc58ee00492d1cd0d79e945aa0639d794
hbizahin <hbizahin@gmail.com>
2024-05-23 02:59:00 +0600

    event summary added

--- DOM/js/events.js
+++ DOM/js/events.js
@@ -7,9 +7,27 @@ function makeRed() {
 }
 
 // system no.3
-const makeBlueButton = document.getElementById('make-blue')
+const makeBlueButton = document.getElementById('make-blue');
 makeBlueButton.onclick = makeBlue();
 
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
+
+// system no.4
+const pinkButton = document.getElementById('make-pink')
+pinkButton.addEventListener('click', makePink);
+function makePink() {
+    document.body.style.backgroundColor = 'pink'
+}
+
+// system no.5
+const makeGreenButton = document.getElementById('make-green')
+makeGreenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+})
+
+// system no.6
+document.getElementById('make-goldenrod').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'goldenrod';
+})
