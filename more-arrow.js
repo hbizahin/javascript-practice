d9add1e588dd4d20d77268175296e374d7d407db
hbizahin <hbizahin@gmail.com>
2024-08-18 03:12:04 +0600

    learned new topics

---
+++ more-arrow.js
@@ -0,0 +1,6 @@
+const difference = (x, y) => x - y;
+const multiply = (first, second, third) => first * second * third;
+const getAge = (person) => person.age
+const student = {name: 'Zahin', age: 22}
+const age = getAge(student)
+console.log(age)
